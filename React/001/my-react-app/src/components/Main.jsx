e8572ccd644fcc43802d8bf3014b9b42ad5acff4
MinKyeongHyeon <28599@naver.com>
2025-08-03 21:49:39 +0900

    study/ 기존 코드를 리팩토링하면서 구조분해할당에 대한 이해를 좀 더 공부함

--- React/001/my-react-app/src/components/Main.jsx
+++ React/001/my-react-app/src/components/Main.jsx
@@ -1,11 +1,36 @@
+import { useEffect, useState } from "react"
 import { Section } from "./Section"
 export function Main (){
+  const [products, setProducts] = useState([]);
+
+    const fetchData = async () =>{
+      try{
+        const response = await fetch('https://dev.wenivops.co.kr/services/fastapi-crud/1/product');
+
+        if(!response.ok){
+          throw new Error(`HTTP error ${response.status}`);
+        }
+
+        const result = await response.json();
+        setProducts(result);
+      }
+      catch{
+        console.error(error);
+        
+      }
+    }
+    
+    useEffect(()=>{
+      fetchData(); //실행할코드
+    },[]); // 배열이 비어있을때실행
+
+
   return (
     <main>
       <p>여기는 메인이지롱!</p>
-      <Section number={1}/>
-      <Section number={2}/>
-      <Section number={3}/>
+      {products.map((product, index)=>{
+        <Section key={product.id || index} number={index + 1} productData={product}/>
+      })}
       
     </main>
   )
--- React/001/my-react-app/src/components/Section.jsx
+++ React/001/my-react-app/src/components/Section.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
-export function Section ({number}){
-  const [data, setData] = useState([]); // state to hold the fetched data
 
+export function Section ({number, productData}){
   function Card(props){
     return(
       <div>
@@ -12,34 +11,17 @@ export function Section ({number}){
     )
   }
 
-  const fetchData = async ()=>{
-    try{
-      const response = await fetch('https://dev.wenivops.co.kr/services/fastapi-crud/1/product');
-      const data = await response.json();
-      if(!response.ok){
-        throw new Error(`HTTP 에러! 상태 ; ${response.status}`);
-      }
-      setData(data); // set the fetched data to state
-    }catch(error){
-      console.log('통신에러',error)
-    }
-  }
-
-  // fetch data when component mounts
-  useEffect(() => {
-    fetchData();
-  }, []);
-
   return (
     <section>
       <h2>여기는 {number}번째 섹션이지롱!</h2>
       <p>이건 섹션에 들어갈 내용이지롱!</p>
-      <>
-      {data.length > 0 && Card({
-        title: data[0].productName,
-        price: data[0].price,
-        imageUrl: data[0].thumbnailImg,
-      })}</>
+      
+      {/* productData가 있으면 Card 렌더링 */}
+      {productData && Card({
+        title: productData.productName,
+        price: productData.price,
+        imageUrl: productData.thumbnailImg,
+      })}
     </section>
   )
 }
