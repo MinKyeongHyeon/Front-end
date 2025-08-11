dfd409d4988b4c092e40fd6630ee04f87d995c63
MinKyeonghyeon <28599@naver.com>
2025-08-11 14:35:11 +0900

    study/customhook 실습

---
+++ React/0811/custom-hook/src/App.jsx
@@ -0,0 +1,14 @@
+import useInput from "../inputComponent"
+import InputComponent from "../somethingComponent"
+
+
+function App() {
+
+    return (
+        <>
+            Hello Vite-React!
+        </>
+    )
+}
+
+export default App
