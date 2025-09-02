a8e968860fdb97a254c62b579c78e518460fdf39
MinKyeonghyeon <28599@naver.com>
2025-09-02 16:05:06 +0900

    vite/ tdd/ 실습 같은걸 함

---
+++ TDD/test.js
@@ -0,0 +1,10 @@
+class textChanger {
+  constructor() {
+    this.txt = "Hello Lions";
+    this.value = this.txt;
+  }
+
+  push(newValue) {
+    this.value = newValue;
+  }
+}
--- Vite/npmTest/app.js
+++ Vite/npmTest/app.js
@@ -1,5 +1,21 @@
-import dog from './dog.png'
+import dog from "./dog.png";
 
-document.addEventListener('DOMContentLoaded',()=>{
-  document.body.innerHTML = `<img src="${dog}">`
-})
+document.addEventListener("DOMContentLoaded", () => {
+  document.body.innerHTML = `<img src="${dog}">`;
+});
+
+class ViewerApp {
+  constructor() {
+    this.txt = "Hello Lions";
+    this.viewer = document.querySelector('.viewer');
+    this.viewer.textContent = this.txt;
+  }
+
+  push() {
+    const inpTxt = document.querySelector('.inp-txt');
+    this.viewer.textContent = inpTxt.value;
+  }
+}
+
+// Jasmine 테스트를 위해 export
+window.ViewerApp = ViewerApp;
